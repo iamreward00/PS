07299d984d603dd221341df3e988fb81d9b94890
Im_reward <121441565+Imreward@users.noreply.github.com>
2025-07-01 16:24:36 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        System.out.print(a);
+        System.out.print("??!");
+        
+
+        
+
+    }
+}
