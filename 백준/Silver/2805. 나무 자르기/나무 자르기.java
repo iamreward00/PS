3a504f21c37f72cf8b88ed3196d9587d7b68826e
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-07-19 16:32:54 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 119424 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int M;
+	static int[] arr;
+
+	public static boolean check(long num){
+		long sum = 0;
+		for(int i = 0; i < N; i++){
+			if(arr[i] >= num){
+				sum += arr[i] - num;
+			}
+		}
+		if( sum >= M ){
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception{
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		long end = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(end < arr[i]){
+				end = arr[i];
+			}
+		}
+		
+		long left = 0;		
+		long mid;
+		
+		while ( left <= end ){
+			mid = (left+end)/2;
+			
+			if(check(mid)){
+				left = mid + 1;
+			}
+			
+			else{
+				end = mid - 1;
+			}
+			
+		}
+		
+		System.out.println(left - 1);
+		br.close();
+
+		
+	}
+}
