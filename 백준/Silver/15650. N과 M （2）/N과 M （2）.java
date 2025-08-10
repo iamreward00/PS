7118a3fcb674688df8894dfb74b3307455027cc9
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-08-10 17:20:26 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,51 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 
-public class Main {
-		static int N,M;
-		static HashSet<String> set = new HashSet<>();
-		
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		find(1,"");
-		ArrayList<String> list = new ArrayList<>(set);
-		Collections.sort(list);
-		for (String ans : list) {
-			for(int i = 0; i < ans.length(); i++) {
-				System.out.print(ans.charAt(i) + " ");
-			}
-			System.out.println();
-		}
-		br.close();
-	}
-	public static void find(int idx, String str) {
-		if(idx > N+1) {
-			return;
-		}
-		if(set.contains(str)) {
-			return;
-		}
-		if(str.length() == M) {
-			set.add(str);
-		}
 
-		
-		find(idx +1,str + (idx+""));
-		find(idx +1, str);
-		
-	}
-	
+/// 백준 15650 - N과 M (2)
+/// https://www.acmicpc.net/problem/15650
+public class Main { 
+    /// M 자리 N 진수라 생각하면 편하긴해
+    /// 출력 길이는 M 개
+    /// 중복 x 순서상관 x 조합 ㄱㄱㄱㄱㄱㄱ
+    /// 
+    static int N,M;
+    static boolean[] vis;
 
+    static int dfs(int idx, int cnt){ // idx 는 현재 보는수 cnt 는 몇개 추가했는지
+        if(cnt == M){
+            for(int i = 1; i <= N; i++){
+                if(vis[i]){
+                    System.out.print(i +" ");
+                }
+            }
+            System.out.println();
+            return 0;
+        }
+        if(idx > N){
+            return 0;
+        }
+
+        vis[idx] = true;
+        dfs(idx+1, cnt+1);
+        vis[idx] = false;
+        dfs(idx+1, cnt);
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        vis = new boolean[N+1]; // 1부터시작이라
+        /// 1부터 시작해야겠네
+        dfs(1,0);
+        }
+    
 }
