a768a9d3f7546fc46b4e6da6da49950762cd2eb1
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-08-10 19:34:48 +0900

    [Silver V] Title: 집합, Time: 968 ms, Memory: 325668 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,61 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.HashMap;
-public class Main{
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		HashMap<Integer, Integer> map = new HashMap<>();
-		int temp = 0;
-        StringBuilder sb = new StringBuilder();
-		HashMap<Integer, Integer> temp_map = new HashMap<>();
-		for(int j = 1; j <= 20; j++) {
-			temp_map.put(j, 1);
-		}
 
-        for( int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			if(str.equals("add")) {
-				temp = Integer.parseInt(st.nextToken());
-				map.put(temp, 1);
-			}
-			
-			else if(str.equals("remove")) {
-				temp = Integer.parseInt(st.nextToken());
-				if(map.containsKey(temp)) {
-					map.remove(temp);					
-				}
-			}
-			
-			else if(str.equals("check")) {
-				temp = Integer.parseInt(st.nextToken());
-				sb.append(map.containsKey(temp) ? 1 : 0).append("\n");
-			}
-			
-			else if(str.equals("toggle")) {
-				temp = Integer.parseInt(st.nextToken());
-				if(map.containsKey(temp)) {
-					map.remove(temp);
-				}
-				else {
-					map.put(temp, 1);
-				}
-			}
-			else if(str.equals("all")) {
-                map = new HashMap<>(temp_map);
-			}
-			else if(str.equals("empty")) {
-				map = new HashMap<>();
-			}
-			
-		
-			
-		}
-		
-		br.close();
-        System.out.print(sb.toString()); 
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(br.readLine());
+        
+        // 집합의 원소 1~20을 표현할 int 변수 (32비트)
+        // 0번째 비트는 숫자 1, 1번째 비트는 숫자 2, ..., 19번째 비트는 숫자 20을 의미
+        int set = 0;
+
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int x = 0;
+            
+            // "all"과 "empty"를 제외한 명령어는 숫자 x를 입력받음
+            if (st.hasMoreTokens()) {
+                x = Integer.parseInt(st.nextToken());
+            }
+
+            switch (command) {
+                case "add":
+                    // x에 해당하는 비트를 1로 변경 (OR 연산)
+                    // 1 << (x-1) : x에 해당하는 비트만 1이고 나머지는 0인 마스크 생성
+                    set |= (1 << (x - 1));
+                    break;
+
+                case "remove":
+                    // x에 해당하는 비트를 0으로 변경 (AND 연산 + NOT 연산)
+                    // ~(1 << (x-1)) : x에 해당하는 비트만 0이고 나머지는 1인 마스크 생성
+                    set &= ~(1 << (x - 1));
+                    break;
+
+                case "check":
+                    // x에 해당하는 비트가 1인지 확인 (AND 연산)
+                    // 연산 결과가 0이 아니면 원소가 존재
+                    if ((set & (1 << (x - 1))) != 0) {
+                        sb.append("1\n");
+                    } else {
+                        sb.append("0\n");
+                    }
+                    break;
+
+                case "toggle":
+                    // x에 해당하는 비트를 반전 (XOR 연산)
+                    // 0이면 1로, 1이면 0으로 변경
+                    set ^= (1 << (x - 1));
+                    break;
+
+                case "all":
+                    // 모든 비트를 1로 변경
+                    // (1 << 20)은 21번째 비트만 1인 수. 여기서 1을 빼면 하위 20개 비트가 모두 1이 됨
+                    set = (1 << 20) - 1;
+                    break;
+
+                case "empty":
+                    // 모든 비트를 0으로 변경
+                    set = 0;
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
 }
