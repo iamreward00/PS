53516e2d64e4bbc08e9b1f315cb2789c7cdb7043
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-08-15 23:31:08 +0900

    [Silver IV] Title: 수 찾기, Time: 496 ms, Memory: 70448 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,73 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+
+
+/// 백준 1920 - 수 찾기
+/// https://www.acmicpc.net/problem/1920
 public class Main {
 
+    static int N,M;
+    static int[] narr;
+    static int[] marr;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
+    static int check(int target){
         
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, 1);
-        }
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            if (map.containsKey(temp)) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-                
+
+        int left = 0;
+        int right = N-1;
+
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(narr[mid] == target){
+                return 1;
             }
+            if(narr[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+
         }
 
-        br.close();
+
+        return 0;
+
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        narr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            narr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        M = Integer.parseInt(br.readLine());
+        marr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            marr[i] = Integer.parseInt(st.nextToken());
+        }
+        /// 입력다받고 이제 정렬부터 시작 ㄱㄱㄱ
+        
+        Arrays.sort(narr);
+        for(int i = 0; i < M; i++){
+            sb.append(check(marr[i]) + "\n");
+        }
+
+        System.out.println(sb);
+
+
     }
 }
