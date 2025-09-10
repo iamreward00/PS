dbcbb7efbda6c92864caf3ac4ac32ead6d775ce5
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-09-10 19:52:47 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,49 +1,48 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M,count;
-    static ArrayList<ArrayList<Integer>> graph;
-    static boolean[] visited;
 
-    static void dfs(int start){
-        visited[start] = true;
-        count += 1;
-        int size = graph.get(start).size();
+    /// 1에서 dfs 돌리면되겠다 bfs나
+    
+    static int N,M;
+    static ArrayList<Integer>[] list;
+    static int cnt;
+    static boolean[] vis;
 
-        for(int i=0; i<size; i++){
-            int vertex = graph.get(start).get(i);
-            if(visited[vertex])
-                continue;
-            dfs(vertex);
+    static int dfs(int idx){
+
+        if(vis[idx]) return 0;
+        vis[idx] = true;
+        int a = 1;
+
+        for(int i : list[idx]){
+            a += dfs(i);
         }
+
+        return a;
     }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());    
+        list = new ArrayList[N+1];
 
-        N = Integer.parseInt(bf.readLine());
-        M = Integer.parseInt(bf.readLine());
-        graph = new ArrayList<ArrayList<Integer>>();
-        visited = new boolean[N+1];
-
-        for(int i=0; i<N+1; i++)
-            graph.add(new ArrayList<Integer>());
-
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(bf.readLine());
+        for(int i = 1; i <= N; i++) list[i] = new ArrayList<>();
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            list[a].add(b);
+            list[b].add(a);
         }
+        vis = new boolean[N+1];
+        
+        System.out.println(dfs(1)-1);
 
-        dfs(1);
-        System.out.println(count-1);
     }
 }
