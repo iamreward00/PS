c3d7e224142d85f06e5a94c615508c9052652583
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-09-25 19:40:19 +0900

    [Gold II] Title: 반도체 설계, Time: 164 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int bis(int num, int[] memo, int len){
+
+        int left = 0;
+        int right = len;    
+
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(num >= memo[mid]){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] memo = new int[N];
+        int len = 1;
+        memo[0] = arr[0];
+        
+        for(int i = 0; i < N; i++){
+            if(memo[len-1] < arr[i]){
+                memo[len++] = arr[i];
+            }
+            else if( memo[len-1] > arr[i]){
+                int idx = bis(arr[i], memo,len);
+                memo[idx] = arr[i];
+            }
+        }
+
+        System.out.println(len);
+
+    }
+}
