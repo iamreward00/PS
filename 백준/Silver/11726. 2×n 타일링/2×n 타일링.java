3dddf356f970102cc1aa27a50bae90ddfd87dd36
Iam_reward <121441565+iamreward00@users.noreply.github.com>
2025-10-19 01:03:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,33 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main{
-
-	public static void main(String[] args) throws Exception{
-
+	
+	
+	public static void main(String[] args)  throws Exception{
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long arr[] = new long[N+5];
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 3;
-		arr[4] = 5;
-		
-		for(int i = 5; i<= N; i++) {
-			arr[i] = ( arr[i-1] + arr[i-2] )%10007;
+		int[] memo = new int[N+3];
+		memo[1] = 1;
+		memo[2] = 2;
+		for(int i = 3; i <= N; i++) {
+			memo[i] = (memo[i-1] + memo[i-2])%10007;
 		}
 		
-		System.out.println(arr[N]);
-		
-		
+		System.out.println(memo[N]);
 		
 	}
-
 }
+/// 9 를 채우기위해서는 8 채우는방식에 세로 하나 하거나  7채우는 방식에 가로가로 하거나 ( 세로세로안하는이유는 7에 세로하는순간 8이되어버려서 8이랑중복이기때문)
 
-// 이문제는 이전에 풀었던 1,2,3 으로 합을 표현하라랑 비슷한데
-// 결국 4는 3에서 1칸더한거 2에서 2칸짜리 더한거 이기 때문에
-// 이전 문제에서는 1 + 3 , 2 + 2, 3 + 1 이였던게 이게 그냥 3+1, 2+2 인거지 왜? 3칸짜리는 의미없으니까 왜? 2+1 이니까 +1, +2 만 계산하는거지
-// 이게 맨앞에있다고 생?각? 해서 하면될듯 ㅇㅇ? 그래서 1+arr[i-1] + 2+arr[i-2] 한거지 ㅇㅇ
+
+
